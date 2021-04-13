a8c1e93f480da0715f9fcc7632bef1c78d968874
ryushin0316 <the.animal.kingdom18@gmail.com>
2021-04-13 13:53:32 +0900

    修正１

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, expect: [:index, :show]
   before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
@@ -14,33 +14,29 @@ class PrototypesController < ApplicationController
   def create
     @prototype = Prototype.new(prototype_params)
     if @prototype.save
-       redirect_to root_path
+      redirect_to root_path
     else
-       render :new
+      render :new
     end
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
-    @comments = @prototype.comments
     @comment = Comment.new
+    @comments = @prototype.comments
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
-      redirect_to root_path
+      redirect_to prototype_path(@prototype)
     else
-     render :edit
+      render :edit
     end
   end
 
   def destroy
-    @prototype = Prototype.find(params[:id])
     if @prototype.destroy
       redirect_to root_path
     else
@@ -48,8 +44,8 @@ class PrototypesController < ApplicationController
     end
   end
 
-
   private
+
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
@@ -60,5 +56,5 @@ class PrototypesController < ApplicationController
 
   def contributor_confirmation
     redirect_to root_path unless current_user == @prototype.user
-    end
   end
+end
